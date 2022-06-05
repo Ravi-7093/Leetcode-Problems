c18959cc7ba2c394e8ba0f47b87c019b6b28cdfc
Ravi_Prakasha <46593559+Ravi-7093@users.noreply.github.com>
2022-06-05 16:41:54 -0500

    Update defang.py

--- 1108. Defanging an IP Address/defang.py
+++ 1108. Defanging an IP Address/defang.py
@@ -1,1 +1,11 @@
-
+class Solution(object):
+    def defangIPaddr(self, address):
+        """
+        :type address: str
+        :rtype: str
+        """
+        x=""
+        for i in address:
+            if (i=="."):
+                x=address.replace(".","[.]")
+                return x
